be9d2229e77a4806404b190a0137585963c840f5
ZayLpursuit <Zaylors@pursuit.org>
2022-11-20 15:50:14 -0500

    more styling

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,6 +1,7 @@
-import { useParams,useNavigate,useState } from "react-router-dom";
+import { useParams,useNavigate, } from "react-router-dom";
 import YouTube, { YouTubeProps } from 'react-youtube';
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "./Sidebar";
 
 export default function Video(){
     const {id}=useParams()
@@ -12,18 +13,38 @@ export default function Video(){
           autoplay: 1,
         },
       };
+
+      const [form,setForm]=useState({name:"",comment:""})
+
+function handleChange(e){
+    setForm({...form,[e.target.id]:e.target.comment})
+}
+
+
+function onSubmit(e){
+e.preventDefault()
+console.log(form.name)
+}
     return (
-    <div>
+    <div >
+        <div className="video-page">
         <YouTube videoId={id} />
-        <form>
-            <label for="name">Name:
-            <input type="text" id="name" value=""/>
+        </div>
+        <form className="comments-form" onSubmit={()=>onSubmit}>
+            <label for="name" className="name-label">Name:
+            <input type="text" id="name" value={form.name} onChange={handleChange}/>
             </label>
             <label for ="comment">Comment:
-            <input type="text" id="comment" value=""/>
+            <input type="text-area" id="comment" value={form.comment} onChange={handleChange}/>
             </label>
+            <input type="submit" value="submit" className="cmmts-submit"/>
         </form>
+        <div className="cmmts-section">
         <h3>Comments</h3>
+        <p>@ {form.name}</p>
+        <p>{form.comment}</p>
+        
+    </div>
     </div>
     )
 }
